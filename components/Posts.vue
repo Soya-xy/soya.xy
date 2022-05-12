fe7192a81aad93f239bf368f08b3d26e2572843a
Soya <3100034521@qq.com>
2022-05-12 18:28:15 +0800

    add post

---
+++ components/Posts.vue
@@ -0,0 +1,18 @@
+<script setup lang='ts'>
+const { showYears = false } = defineProps<{ showYears?: boolean }>()
+const yearOnly = $ref('')
+</script>
+
+<template>
+  <div>
+    <section v-if="showYears" key="{year}">
+      <h2>{{ yearOnly }}</h2>
+      <div class="posts">
+        <!-- <Post key="{node.id}" node="{node}" query="{query}" prefix="{prefix}" /> -->
+      </div>
+    </section>
+    <div v-else class="posts">
+      <!-- <Post key={node.id} node={node} query={query} prefix={prefix} hideDate={hideDate} yearOnly={yearOnly} /> -->
+    </div>
+  </div>
+</template>
